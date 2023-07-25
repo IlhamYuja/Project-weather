ba0b20786cc504b3144fd6207377e0fac2871a61
ilham yuja <ilhamyuja@gmail.com>
2023-07-25 07:44:44 +0700

    add API function

---
+++ src/components/api.js
@@ -0,0 +1,10 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'bae7133c67msh38e47404250016ep128c6ejsn06f60d0caa0f',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
