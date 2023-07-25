f984a24425581f539a7603f7882f17c0d32c4917
ilham yuja <ilhamyuja@gmail.com>
2023-07-25 15:58:09 +0700

    fetching and mapping data from weather API

--- src/components/api.js
+++ src/components/api.js
@@ -6,5 +6,8 @@ export const geoApiOptions = {
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
 
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+export const WEATHER_API_KEY = 'b5bd932397586c6260f55ed6b32bd45c'
